d3ad6b8154bc580e75079af52cbc217dfec056f1
Voices <102823613+criosfera@users.noreply.github.com>
2023-06-23 06:12:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def lista():
     return render_template("lista.html", rows=rows)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4000, host="0.0.0.0")
+    app.run(debug=True, port=5000, host="0.0.0.0")
